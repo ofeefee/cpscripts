73887355cd7eef3b38fbf091cc57489b0b606473
ofeefee <ofeefee@gmail.com>
2019-08-01 19:02:01 -0500

    added quick edit variables
    allows for changing of api port
    allows for changing export file
    allows for controlling output (condensed default)

--- cpinfo_pull.sh
+++ cpinfo_pull.sh
@@ -1,17 +1,53 @@
 #!/bin/bash
 
-FILE="myfile.txt"
+# -----------------------------------
+outfile="myfile.txt"
+mgmtport="4434"
+outputformat="1" #1=condensed#
+# -----------------------------------
 
-mapfile -t my_array < <( mgmt_cli -r true show gateways-and-servers --port 4434 --format json | jq '.objects[] | select(.type=="simple-gateway") | .name' )
+# Pull objects
+echo "pulling objects"
+objectjson=$(mgmt_cli -r true show gateways-and-servers --port $mgmtport --format json)
 
-if [ -f "$FILE" ]
+# Create target list
+echo "creating target list"
+mapfile -t gwarray < <(echo $objectjson | jq '.objects[] | select(.type=="simple-gateway") | .name' )
+
+# Delete output file if exists
+if [ -f "$outfile" ]
 then
-     rm myfile.txt
+     rm $outfile
 fi
 
-for name in ${my_array[@]}; do
-     echo $name >> myfile.txt
-     mgmt_cli -r true run-script script-name "run cpinfo" script "cpinfo -y all /" targets.1 "$name" --port 4434  --format json 2> /dev/null | $CPDIR/jq/jq -r '(.tasks[0]["task-details"][0].responseMessage)' | base64 -di >> myfile.txt
+echo "running script"
+for name in ${gwarray[@]}; do
 
+# Run script
+scriptjson=$(mgmt_cli -r true run-script script-name "run cpinfo" script "cpinfo -y all /" targets.1 "$name" --port $mgmtport  --format json 2> /dev/null)
+status=$(echo $scriptjson | jq -r '.tasks[0]["task-details"][0].statusCode')
+statusdesc=$(echo $scriptjson | jq -r '.tasks[0]["task-details"][0].statusDescription')
+response=$(echo $scriptjson | $CPDIR/jq/jq -r '(.tasks[0]["task-details"][0].responseMessage)' | base64 -di)
+
+if [ "$status" = "failed" ]
+then
+     echo $name, $status, $statusdesc
+
+     # add to output file
+     echo $name, $status, $statusdesc >> $outfile
+else
+     echo $name, $status
+
+     # add to output file
+     echo $name, $status >> $outfile
+
+     if [ "$outputformat" = 1 ]
+     then
+          echo $response >> $outfile
+     else
+          echo $scriptjson | $CPDIR/jq/jq -r '(.tasks[0]["task-details"][0].responseMessage)' | base64 -di >> $outfile     
+     fi
+#    mgmt_cli -r true run-script script-name "run cpinfo" script "cpinfo -y all /" targets.1 "$name" --port $mgmtport  --format json 2> /dev/null | $CPDIR/jq/jq -r '(.tasks[0]["task-details"][0].responseMessage)' | base64 -di >> $outfile
+fi
 done
 
