095174b0f6988a4eec2e5bcc6f9455dd719d3544
ofeefee <ofeefee@gmail.com>
2019-08-01 19:08:49 -0500

    some cleanup

--- cpinfo_pull.sh
+++ cpinfo_pull.sh
@@ -3,16 +3,8 @@
 # -----------------------------------
 outfile="myfile.txt"
 mgmtport="4434"
-outputformat="1" #1=condensed#
+outputformat="1"
 # -----------------------------------
-
-# Pull objects
-echo "pulling objects"
-objectjson=$(mgmt_cli -r true show gateways-and-servers --port $mgmtport --format json)
-
-# Create target list
-echo "creating target list"
-mapfile -t gwarray < <(echo $objectjson | jq '.objects[] | select(.type=="simple-gateway") | .name' )
 
 # Delete output file if exists
 if [ -f "$outfile" ]
@@ -20,34 +12,36 @@ then
      rm $outfile
 fi
 
-echo "running script"
-for name in ${gwarray[@]}; do
+# Pull objects
+echo "pulling objects"
+objjson=$(mgmt_cli -r true show gateways-and-servers --port $mgmtport --format json)
+
+# Create target list
+echo "creating target list"
+mapfile -t objarray < <(echo $objjson | jq '.objects[] | select(.type=="simple-gateway") | .name' )
 
 # Run script
-scriptjson=$(mgmt_cli -r true run-script script-name "run cpinfo" script "cpinfo -y all /" targets.1 "$name" --port $mgmtport  --format json 2> /dev/null)
-status=$(echo $scriptjson | jq -r '.tasks[0]["task-details"][0].statusCode')
-statusdesc=$(echo $scriptjson | jq -r '.tasks[0]["task-details"][0].statusDescription')
-response=$(echo $scriptjson | $CPDIR/jq/jq -r '(.tasks[0]["task-details"][0].responseMessage)' | base64 -di)
+echo "running script"
+for name in ${objarray[@]}; do
+     scriptjson=$(mgmt_cli -r true run-script script-name "run cpinfo" script "cpinfo -y all /" targets.1 "$name" --port $mgmtport  --format json 2> /dev/null)
+     status=$(echo $scriptjson | jq -r '.tasks[0]["task-details"][0].statusCode')
+     statusdesc=$(echo $scriptjson | jq -r '.tasks[0]["task-details"][0].statusDescription')
+     response=$(echo $scriptjson | $CPDIR/jq/jq -r '(.tasks[0]["task-details"][0].responseMessage)' | base64 -di)
 
-if [ "$status" = "failed" ]
-then
-     echo $name, $status, $statusdesc
-
-     # add to output file
-     echo $name, $status, $statusdesc >> $outfile
-else
-     echo $name, $status
-
-     # add to output file
-     echo $name, $status >> $outfile
-
-     if [ "$outputformat" = 1 ]
+     if [ "$status" = "failed" ]
      then
-          echo $response >> $outfile
+          echo $name, $status, $statusdesc
+          # add to output file
+          echo $name, $status, $statusdesc >> $outfile
      else
-          echo $scriptjson | $CPDIR/jq/jq -r '(.tasks[0]["task-details"][0].responseMessage)' | base64 -di >> $outfile     
+          echo $name, $status
+          # add to output file
+          echo $name, $status >> $outfile
+          if [ "$outputformat" = 1 ]
+          then
+               echo $response >> $outfile
+          else
+               echo $scriptjson | $CPDIR/jq/jq -r '(.tasks[0]["task-details"][0].responseMessage)' | base64 -di >> $outfile     
+          fi
      fi
-#    mgmt_cli -r true run-script script-name "run cpinfo" script "cpinfo -y all /" targets.1 "$name" --port $mgmtport  --format json 2> /dev/null | $CPDIR/jq/jq -r '(.tasks[0]["task-details"][0].responseMessage)' | base64 -di >> $outfile
-fi
 done
-
